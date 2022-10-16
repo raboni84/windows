36169aa8696fe209368d245ab260bf6f8def68d7
windows <windows>
2022-10-16 11:22:51 +0200

    debugging multithreaded download of symbols

--- debugger.ps1
+++ debugger.ps1
@@ -1,9 +1,9 @@
 # WinDbg Preview
 Write-Host "Trying to find msixbundle url for windbg preview"
-$WebResponse = Invoke-WebRequest -Method 'POST' -Uri 'https://store.rg-adguard.net/api/GetFiles' -Body "type=PackageFamilyName&url=Microsoft.WinDbg_8wekyb3d8bbwe&ring=Retail" -ContentType 'application/x-www-form-urlencoded' -UseBasicParsing
+$WebResponse = Invoke-WebRequest -Method 'POST' -Uri 'https://store.rg-adguard.net/api/GetFiles' -Body "type=PackageFamilyName&url=Microsoft.WinDbg_8wekyb3d8bbwe&ring=Retail" -ContentType 'application/x-www-form-urlencoded' -UseBasicParsing -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0"
 $LinkMatch = $WebResponse.Links | where { $_ -like '*_neutral*.msixbundle*' } | Select-String -Pattern '(?<=a href=").+(?=" r)' | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value
 Write-Host "Downloading from $($LinkMatch)"
-Invoke-WebRequest -Uri $LinkMatch -OutFile "$ENV:USERPROFILE/Desktop/windbg.msixbundle" -UseBasicParsing
+Invoke-WebRequest -Uri $LinkMatch -OutFile "$ENV:USERPROFILE/Desktop/windbg.msixbundle" -UseBasicParsing -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0"
 Write-Host "Installing $ENV:USERPROFILE/Desktop/windbg.msixbundle"
 Add-AppxPackage -Path "$ENV:USERPROFILE/Desktop/windbg.msixbundle" -RequiredContentGroupOnly
 Write-Host "Done"
@@ -13,22 +13,28 @@ Start-Process -NoNewWindow -Wait -FilePath "E:/winsdksetup.exe" -WorkingDirector
 
 # Rizin
 Write-Host "Downloading vcruntime140"
-Invoke-WebRequest -Uri "https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe" -OutFile "$ENV:USERPROFILE/Desktop/vc_installer.exe" -UseBasicParsing
+Invoke-WebRequest -Uri "https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe" -OutFile "$ENV:USERPROFILE/Desktop/vc_installer.exe" -UseBasicParsing -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0"
 Write-Host "Start installing vcruntime140"
 Start-Process -NoNewWindow -Wait -FilePath "$ENV:USERPROFILE/Desktop/vc_installer.exe" -WorkingDirectory "$ENV:USERPROFILE/Desktop" -ArgumentList "/SILENT","/ALLUSERS","/NORESTART"
 
 Write-Host "Downloading rizin"
-Invoke-WebRequest -Uri "https://github.com/rizinorg/rizin/releases/download/v0.4.1/rizin_installer-v0.4.1-x86_64.exe" -OutFile "$ENV:USERPROFILE/Desktop/rizin_installer.exe" -UseBasicParsing
+Invoke-WebRequest -Uri "https://github.com/rizinorg/rizin/releases/download/v0.4.1/rizin_installer-v0.4.1-x86_64.exe" -OutFile "$ENV:USERPROFILE/Desktop/rizin_installer.exe" -UseBasicParsing -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0"
 Write-Host "Start installing rizin"
 Start-Process -NoNewWindow -Wait -FilePath "$ENV:USERPROFILE/Desktop/rizin_installer.exe" -WorkingDirectory "$ENV:USERPROFILE/Desktop" -ArgumentList "/SILENT","/ALLUSERS","/NORESTART"
 
 # Symbols for Windows 10
 New-Item -Path "$ENV:WINDIR" -Name "SYMBOLS" -ItemType "directory" -Force | Out-Null
-$files = Get-ChildItem "$ENV:WINDIR" -recurse | Where { $_.extension -in ".exe",".dll",".sys" } | Select-Object -ExpandProperty FullName
+$syspaths = "$ENV:WINDIR","$ENV:WINDIR/System32","$ENV:WINDIR/System32/Drivers","$ENV:WINDIR/SysWOW64"
+$files = Get-ChildItem $syspaths | Where { $_.Extension -in ".exe",".dll",".sys" } | Select-Object -ExpandProperty FullName
+$filecnt = $files.Count
 $jobs = @()
+$idx = 0
 foreach ($file in $files) {
+  $idx += 1
   $jobs += Start-ThreadJob -ScriptBlock {
     $jobfile = $using:file
+    $jobidx = $using:idx
+    $jobcnt = $using:filecnt
     $pdb = & "C:\Program Files\Rizin\bin\rz-bin.exe" -I $jobfile | Select-String -Pattern 'dbg_file +([^$]+)' | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Groups | Select-Object -ExpandProperty Value -Skip 1
     $guid = & "C:\Program Files\Rizin\bin\rz-bin.exe" -I $jobfile | Select-String -Pattern 'guid +([0-9a-fA-F]+)' | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Groups | Select-Object -ExpandProperty Value -Skip 1
     if ($pdb -And $guid) {
@@ -44,20 +50,20 @@ foreach ($file in $files) {
           $StatusCode = $_.Exception.Response.StatusCode.value__
         }
         if ($StatusCode -eq 200) {
-          Write-Host "$($pdb) => $($locallink)"
+          Write-Host "[$($jobidx)/$($jobcnt)] $($jobfile) => $($locallink)"
           New-Item -Path "$ENV:WINDIR/SYMBOLS" -Name $pdb -ItemType "directory" -Force | Out-Null
           New-Item -Path "$ENV:WINDIR/SYMBOLS/$($pdb)" -Name $guid -ItemType "directory" -Force | Out-Null
-          # ask again because of multithreading
+          # ask again because of multithreading and filename changes
           if (-Not(Test-Path -Path $locallink -PathType Leaf)) {
             Invoke-WebRequest -Uri $linktodbg -OutFile $locallink -UseBasicParsing -UserAgent "Microsoft-Symbol-Server/10.0.10240.9"
-            Add-Content "$ENV:USERPROFILE/Desktop/symbols_success.txt" "$($_.FullName),$($pdb),$($guid)"
+            Add-Content "$ENV:USERPROFILE/Desktop/symbols_success.txt" "$($jobfile),$($pdb),$($guid)"
           }
         } else {
-          Add-Content "$ENV:USERPROFILE/Desktop/symbols_failure.txt" "$($_.FullName),#$($StatusCode)"
+          Add-Content "$ENV:USERPROFILE/Desktop/symbols_failure.txt" "$($jobfile),#$($StatusCode)"
         }
       }
     } else {
-      Add-Content "$ENV:USERPROFILE/Desktop/symbols_failure.txt" "$($_.FullName),#nopdborguid"
+      Add-Content "$ENV:USERPROFILE/Desktop/symbols_failure.txt" "$($jobfile),#nopdborguid"
     }
   } -StreamingHost $Host
 }
